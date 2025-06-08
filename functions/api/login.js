bb59b67ba3b5778ccecb9f952906348905ef2137
openjsw 开放技术 <toewpq@gmail.com>
2025-06-08 15:32:59 +0800

    Update login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,17 +1,14 @@
 import { sign } from '../utils/jwt';
 
-const ADMIN_USER = 'env.admin';
-const ADMIN_PASS = 'env.password'; // 强烈建议环境变量读取
-
-export async function onRequestPost({ request }) {
+export async function onRequestPost({ request, env }) {
   try {
     const { user, pass } = await request.json();
-    if (user === ADMIN_USER && pass === ADMIN_PASS) {
-      const token = await sign({ user, admin: true });
+    if (user === env.ADMIN_USER && pass === env.ADMIN_PASS) {
+      const token = await sign({ user, admin: true }, env.JWT_SECRET);
       return Response.json({ token });
     }
     return Response.json({ error: '用户名或密码错误' }, { status: 403 });
   } catch {
-    return Response.json({ error: '请求数据格式错误' }, { status: 400 });
+    return Response.json({ error: '请求格式错误' }, { status: 400 });
   }
 }
