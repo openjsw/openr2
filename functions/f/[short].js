a83f717faac30d5bc634537b9d92ca5c9e84378a
openjsw 开放技术 <toewpq@gmail.com>
2025-06-08 15:35:38 +0800

    Update [short].js

--- functions/f/[short].js
+++ functions/f/[short].js
@@ -1,7 +1,3 @@
-/**
- * 短链跳转
- * - 访问 /f/xxxx 会自动 302 跳转到真实文件直链（带友好报错）
- */
 export async function onRequestGet({ params, env }) {
   const short = params.short;
   if (!short) return new Response('短链参数缺失', { status: 400 });
@@ -9,6 +5,5 @@ export async function onRequestGet({ params, env }) {
   const fileId = await env.MY_KV.get(`short:${short}`);
   if (!fileId) return new Response('短链不存在或已失效', { status: 404 });
 
-  // 这里直接重定向到 /file/xxx
   return Response.redirect(`/file/${fileId}`, 302);
 }
